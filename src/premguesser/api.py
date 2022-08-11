20d8c5b4c34bb1d86f2ad2901ff6b33b6d6d665c
MiniEggz <owenscook1@gmail.com>
2022-08-11 14:13:47 +0100

    Move player functionality into objects.

--- src/premguesser/api.py
+++ src/premguesser/api.py
@@ -3,7 +3,7 @@ from typing import List
 
 import requests
 
-from objects import Table
+from objects import AllPlayers, Player, Table
 
 response = requests.get(url="https://fantasy.premierleague.com/api/bootstrap-static/")
 
@@ -56,123 +56,8 @@ fixtures = requests.get("https://fantasy.premierleague.com/api/fixtures/").json(
 
 # element = player?
 # element type 1, 2, 3, 4 == gk, def, mid, for
-players = response.json().get("elements")
-strip_keys(
-    players,
-    [
-        "id",
-        "first_name",
-        "second_name",
-        "goals_scored",
-        "assists",
-        "clean_sheets",
-        "element_type",
-    ],
-)
-
-
-def get_top_goals_scored(players: List[dict]) -> int:
-    """Get the highest number of goals scored by a single player.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        int: most goals scored by a single player.
-    """
-    most_goals = 0
-    for player in players:
-        if player["goals_scored"] > most_goals:
-            most_goals = player["goals_scored"]
-    return most_goals
-
-
-def get_top_scorers(players: List[dict]) -> List[dict]:
-    """Get list of top scorers.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        List[dict]: list of all players that have the highest number of goals.
-    """
-    most_goals = get_top_goals_scored(players)
-    return [player for player in players if player["goals_scored"] == most_goals]
-
-
-def get_top_assists_number(players: List[dict]) -> int:
-    """Get the highest number of assists made by a single player.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        int: most assists made by a single player.
-    """
-    most_assists = 0
-    for player in players:
-        if player["assists"] > most_assists:
-            most_assists = player["assists"]
-    return most_assists
-
-
-def get_top_assisters(players: List[dict]) -> List[dict]:
-    """Get list of top assisters.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        List[dict]: list of all players that have the highest number of assists.
-    """
-    most_assists = get_top_assists_number(players)
-    return [player for player in players if player["assists"] == most_assists]
-
-
-def get_most_clean_sheets_by_keeper(players: List[dict]) -> int:
-    """Get the highest number of clean sheets by a single keeper.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        int: highest number of clean sheets kepy by a keeper.
-    """
-    most_clean_sheets = 0
-    for player in players:
-        if player["element_type"] == 1 and player["clean_sheets"] > most_clean_sheets:
-            most_clean_sheets = player["clean_sheets"]
-    return most_clean_sheets
-
-
-def get_keepers_with_most_clean_sheets(players: List[dict]) -> List[dict]:
-    """Get the keepers with the most clean sheets.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        List[dict]: list of all keepers with highest number of clean sheets.
-    """
-
-    most_clean_sheets = get_most_clean_sheets_by_keeper(players)
-    return [
-        player
-        for player in players
-        if player["clean_sheets"] == most_clean_sheets and player["element_type"] == 1
-    ]
-
-
-def rip_player_names(players: List[dict]) -> List[str]:
-    """Get list of player names from player dictionary.
-
-    Args:
-        players (List[dict]): list of all players and their data.
-
-    Returns:
-        List[str]: list of player names.
-    """
-    return [f"{player['first_name']} {player['second_name']}" for player in players]
+players_response = response.json().get("elements")
+players = AllPlayers(players_response)
 
 
 # premier league website and fpl use different names for teams...
@@ -207,16 +92,17 @@ for team in teams:
 league_table = Table(teams)
 league_table.display_table()
 
-print(f"Top goal scorer(s) with {get_top_goals_scored(players)} goal(s): ")
-for player_name in rip_player_names(get_top_scorers(players)):
-    print("\t" + player_name)
 
-print(f"Top assister(s) with {get_top_assists_number(players)} assist(s):")
-for player_name in rip_player_names(get_top_assisters(players)):
-    print("\t" + player_name)
+print(f"Top goal scorer(s) with {players.get_top_goals_scored()} goal(s): ")
+for player in players.get_top_scorers():
+    print("\t" + player.name)
+
+print(f"Top assister(s) with {players.get_top_assists_number()} assist(s):")
+for player in players.get_top_assisters():
+    print("\t" + player.name)
 
 print(
-    f"Keeper(s) with the most clean sheets with {get_most_clean_sheets_by_keeper(players)} clean sheet(s):"
+    f"Keeper(s) with the most clean sheets with {players.get_top_assists_number()} clean sheet(s):"
 )
-for keeper_name in rip_player_names(get_keepers_with_most_clean_sheets(players)):
-    print("\t" + keeper_name)
+for keeper in players.get_keepers_with_most_clean_sheets():
+    print("\t" + keeper.name)
--- src/premguesser/objects.py
+++ src/premguesser/objects.py
@@ -1,6 +1,9 @@
 """Objects for premier league results guesser."""
 # will contain objects used for creating guesses, etc.
 from functools import cmp_to_key
+from typing import List
+
+from pydantic import BaseModel
 
 # PremState
 # dict containing league positions
@@ -62,3 +65,95 @@ class Table:
                             return -1
                         else:
                             return 1
+
+
+class Player(BaseModel):
+    """Hold information about each player."""
+
+    id: int
+    first_name: str
+    second_name: str
+    goals_scored: int
+    assists: int
+    clean_sheets: int
+    element_type: int
+
+    @property
+    def name(self):
+        """Full name of player."""
+        return f"{self.first_name} {self.second_name}"
+
+
+class AllPlayers:
+    """Holds all players."""
+
+    def __init__(self, players_response: dict):
+        self.players = [Player(**player) for player in players_response]
+
+    def get_top_goals_scored(self) -> int:
+        """Get the highest number of goals scored by a single player.
+
+        Returns:
+            int: most goals scored by a single player.
+        """
+        most_goals = 0
+        for player in self.players:
+            if player.goals_scored > most_goals:
+                most_goals = player.goals_scored
+        return most_goals
+
+    def get_top_scorers(self) -> List[dict]:
+        """Get list of top scorers.
+
+        Returns:
+            List[dict]: list of all players that have the highest number of goals.
+        """
+        most_goals = self.get_top_goals_scored()
+        return [player for player in self.players if player.goals_scored == most_goals]
+
+    def get_top_assists_number(self) -> int:
+        """Get the highest number of assists made by a single player.
+
+        Returns:
+            int: most assists made by a single player.
+        """
+        most_assists = 0
+        for player in self.players:
+            if player.assists > most_assists:
+                most_assists = player.assists
+        return most_assists
+
+    def get_top_assisters(self) -> List[dict]:
+        """Get list of top assisters.
+
+        Returns:
+            List[dict]: list of all players that have the highest number of assists.
+        """
+        most_assists = self.get_top_assists_number()
+        return [player for player in self.players if player.assists == most_assists]
+
+    def get_most_clean_sheets_by_keeper(self) -> int:
+        """Get the highest number of clean sheets by a single keeper.
+
+        Returns:
+            int: highest number of clean sheets kepy by a keeper.
+        """
+        most_clean_sheets = 0
+        for player in self.players:
+            if player.element_type == 1 and player.element_type > most_clean_sheets:
+                most_clean_sheets = player.element_type
+        return most_clean_sheets
+
+    def get_keepers_with_most_clean_sheets(self) -> List[dict]:
+        """Get the keepers with the most clean sheets.
+
+        Returns:
+            List[dict]: list of all keepers with highest number of clean sheets.
+        """
+
+        most_clean_sheets = self.get_most_clean_sheets_by_keeper()
+        return [
+            player
+            for player in self.players
+            if player.clean_sheets == most_clean_sheets and player.element_type == 1
+        ]
