422c7803306f0b09391999a8079cc2bf02c51b09
MiniEggz <owenscook1@gmail.com>
2022-08-12 01:40:39 +0100

    Update objects to tidy up api.py.

--- src/premguesser/api.py
+++ src/premguesser/api.py
@@ -3,95 +3,20 @@ from typing import List
 
 import requests
 
-from objects import AllPlayers, Player, Table
+from objects import AllPlayers, Fixture, League, Player, Team
 
 response = requests.get(url="https://fantasy.premierleague.com/api/bootstrap-static/")
 
-# events looks like gameweeks
-events = response.json().get("events")
+teams_raw = response.json().get("teams")
+fixtures_raw = requests.get("https://fantasy.premierleague.com/api/fixtures/").json()
+players_raw = response.json().get("elements")
 
-# teams
-teams = response.json().get("teams")
+teams = [Team(**team) for team in teams_raw]
+fixtures = [Fixture(**fixture) for fixture in fixtures_raw]
+league = League(teams=teams, fixtures=fixtures)
+league.display_table()
 
-
-def strip_keys(dict_list: List[dict], key_whitelist: List[str]):
-    """Strip back to only relevant team data.
-
-    Args:
-        dict_list (List[dict]): list of dicts to strip.
-        key_whitelist (List[str]): list of keys that are relevant.
-
-    Returns:
-        List[dict]: stripped back list of dicts.
-    """
-    # get keys to remove
-    remove_keys = [key for key in dict_list[0].keys() if key not in key_whitelist]
-
-    # remove keys
-    for dic in dict_list:
-        for key in remove_keys:
-            del dic[key]
-
-
-def reset_stats(teams: List[dict]):
-    """Set all points/goals to 0.
-
-    Args:
-        teams (List[dict]): list of teams to have stats added.
-    """
-    for team in teams:
-        team["points"] = 0
-        team["goals_for"] = 0
-        team["goals_against"] = 0
-
-
-# get all the team names
-team_names = {team.get("name"): team.get("id") for team in teams}
-
-
-team_points = [team.get("points") for team in teams]
-
-# fixtures
-fixtures = requests.get("https://fantasy.premierleague.com/api/fixtures/").json()
-
-# element = player?
-# element type 1, 2, 3, 4 == gk, def, mid, for
-players_response = response.json().get("elements")
-players = AllPlayers(players_response)
-
-
-# premier league website and fpl use different names for teams...
-team_points = [0 for i in range(20)]
-strip_keys(teams, ["id", "name", "short_name"])
-reset_stats(teams)
-
-for fixture in fixtures:
-    try:
-        # update home team goals for/against
-        teams[fixture["team_h"] - 1]["goals_for"] += fixture["team_h_score"]
-        teams[fixture["team_h"] - 1]["goals_against"] += fixture["team_a_score"]
-        # update away team goals for/against
-        teams[fixture["team_a"] - 1]["goals_for"] += fixture["team_a_score"]
-        teams[fixture["team_a"] - 1]["goals_against"] += fixture["team_h_score"]
-
-        if fixture["team_h_score"] > fixture["team_a_score"]:
-            teams[fixture["team_h"] - 1]["points"] += 3
-        elif fixture["team_h_score"] < fixture["team_a_score"]:
-            teams[fixture["team_a"] - 1]["points"] += 3
-        else:
-            teams[fixture["team_h"] - 1]["points"] += 1
-            teams[fixture["team_a"] - 1]["points"] += 1
-    except TypeError:
-        pass
-
-# calculate goal difference for each team
-for team in teams:
-    team["goal_difference"] = team["goals_for"] - team["goals_against"]
-
-
-league_table = Table(teams)
-league_table.display_table()
-
+players = AllPlayers(players_raw)
 
 print(f"Top goal scorer(s) with {players.get_top_goals_scored()} goal(s): ")
 for player in players.get_top_scorers():
@@ -102,7 +27,7 @@ for player in players.get_top_assisters():
     print("\t" + player.name)
 
 print(
-    f"Keeper(s) with the most clean sheets with {players.get_top_assists_number()} clean sheet(s):"
+    f"Keeper(s) with the most clean sheets with {players.get_most_clean_sheets_by_keeper()} clean sheet(s):"
 )
 for keeper in players.get_keepers_with_most_clean_sheets():
     print("\t" + keeper.name)
--- src/premguesser/objects.py
+++ src/premguesser/objects.py
@@ -1,70 +1,15 @@
 """Objects for premier league results guesser."""
 # will contain objects used for creating guesses, etc.
+from dataclasses import dataclass
+from datetime import datetime
 from functools import cmp_to_key
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 # PremState
 # dict containing league positions
 # variables holding other statistics
-
-
-class Table:
-    """Object to represent a league table."""
-
-    def __init__(self, teams: dict):
-        self.teams = teams.copy()
-
-    def get_table(self):
-        """Get the current table."""
-        return sorted(self.teams, key=cmp_to_key(self._compare))
-
-    def display_table(self):
-        """Display the current table."""
-        current_table = self.get_table()
-        print("TEAM\tGD\tGF\tGA\tPOINTS")
-        print("===============================================")
-        for team in current_table:
-            print(team["short_name"], end="\t")
-            print(team["goal_difference"], end="\t")
-            print(team["goals_for"], end="\t")
-            print(team["goals_against"], end="\t")
-            print(team["points"])
-        print("===============================================")
-
-    @staticmethod
-    def _compare(team1, team2):
-        """Compare teams as leagues do."""
-        # compare points
-        if team1["points"] < team2["points"]:
-            return 1
-        elif team1["points"] > team2["points"]:
-            return -1
-        else:
-            # compare goal difference
-            if team1["goal_difference"] < team2["goal_difference"]:
-                return 1
-            elif team1["goal_difference"] > team2["goal_difference"]:
-                return -1
-            else:
-                # compare goals for
-                if team1["goals_for"] < team2["goals_for"]:
-                    return 1
-                elif team2["goals_for"] > team2["goals_for"]:
-                    return -1
-                else:
-                    # compare goals against
-                    if team1["goals_against"] < team2["goals_against"]:
-                        return -1
-                    elif team2["goals_against"] > team2["goals_against"]:
-                        return 1
-                    else:
-                        # default to alphabet
-                        if team1["name"] < team2["name"]:
-                            return -1
-                        else:
-                            return 1
 
 
 class Player(BaseModel):
@@ -157,3 +102,141 @@ class AllPlayers:
             for player in self.players
             if player.clean_sheets == most_clean_sheets and player.element_type == 1
         ]
+
+
+class Team(BaseModel):
+    """Hold information about a team."""
+
+    # TODO: add games_played (or equivalent)
+    id: int
+    name: str
+    short_name: str
+    points: int
+    goals_for: int = 0
+    goals_against: int = 0
+
+    def __post_init__(self):
+        self.reset_stats()
+
+    @property
+    def goal_difference(self):
+        """Calculated goal difference."""
+        return self.goals_for - self.goals_against
+
+    def reset_stats(self):
+        """Reset points and goal statistics."""
+        self.points = 0
+        self.goals_for = 0
+        self.goals_against = 0
+
+
+class Fixture(BaseModel):
+    """Object to represent fixtures."""
+
+    id: int
+    team_h: int
+    team_h_score: Optional[int]
+    team_a: int
+    team_a_score: Optional[int]
+    minutes: int
+    kickoff_time: datetime
+    finished: bool
+
+
+@dataclass
+class League:
+    """Object to represent a league."""
+
+    teams: List[Team]
+    fixtures: List[Fixture]
+
+    def __post_init__(self):
+        self.calculate_team_stats()
+
+    def get_table(self):
+        """Get the current table."""
+        return sorted(self.teams, key=cmp_to_key(self._compare))
+
+    def display_table(self):
+        """Display the current table."""
+        current_table = self.get_table()
+        print("TEAM\tGD\tGF\tGA\tPOINTS")
+        print("===============================================")
+        for team in current_table:
+            print(team.short_name, end="\t")
+            print(team.goal_difference, end="\t")
+            print(team.goals_for, end="\t")
+            print(team.goals_against, end="\t")
+            print(team.points)
+        print("===============================================")
+
+    def calculate_team_stats(self):
+        """Calculate the points and score statistics."""
+        for fixture in self.fixtures:
+            try:
+                home_team = self.get_team_with_id(fixture.team_h)
+                away_team = self.get_team_with_id(fixture.team_a)
+                # update home team goals for/against
+                home_team.goals_for += fixture.team_h_score
+                home_team.goals_against += fixture.team_a_score
+                # update away team goals for/against
+                away_team.goals_for += fixture.team_a_score
+                away_team.goals_against += fixture.team_h_score
+
+                if fixture.team_h_score > fixture.team_a_score:
+                    home_team.points += 3
+                elif fixture.team_h_score < fixture.team_a_score:
+                    away_team.points += 3
+                else:
+                    home_team.points += 1
+                    away_team.points += 1
+            # TODO: add games played.
+            except TypeError:
+                pass
+
+    def get_team_with_id(self, team_id: int) -> Team:
+        """Get team with given id.
+
+        Args:
+            team_id (int): id of a team.
+
+        Returns:
+            Team: team with the given id.
+        """
+        for team in self.teams:
+            if team.id == team_id:
+                return team
+
+    @staticmethod
+    def _compare(team1: Team, team2: Team):
+        """Compare teams as leagues do."""
+
+        # compare points
+        if team1.points < team2.points:
+            return 1
+        elif team1.points > team2.points:
+            return -1
+        else:
+            # compare goal difference
+            if team1.goal_difference < team2.goal_difference:
+                return 1
+            elif team1.goal_difference > team2.goal_difference:
+                return -1
+            else:
+                # compare goals for
+                if team1.goals_for < team2.goals_for:
+                    return 1
+                elif team2.goals_for > team2.goals_for:
+                    return -1
+                else:
+                    # compare goals against
+                    if team1.goals_against < team2.goals_against:
+                        return -1
+                    elif team2.goals_against > team2.goals_against:
+                        return 1
+                    else:
+                        # default to alphabet
+                        if team1.name < team2.name:
+                            return -1
+                        else:
+                            return 1
